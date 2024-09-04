de2977c4607a0c06415516827454e900ca398d9c
Natthawan-Lobla <natthawan.lobla@gmail.com>
2024-09-04 11:41:35 +0700

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,6 @@
+console.log("Hello JavaScript");
+console.log("Hello Thursday");
+document.getElementById("text").innerHTML = "Natthawan Lobla"; /*เรียกใช้ id ชื่อ text*/
+document.write("Hello document write");/*แสดงผลที่ console*/
+
+window.alert("Welcome to my page");/*แสดงผลที่หน้าต่าง*/
