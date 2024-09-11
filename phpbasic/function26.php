544a3615ab933c9c371d3f21cd0b3dc7ad1cdaa3
Natthawan-Lobla <natthawan.lobla@gmail.com>
2024-09-11 12:04:04 +0700

    Add files via upload

---
+++ phpbasic/function26.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WriteMessage</title>
+</head>
+<body>
+    <?php
+        /*defining a php function*/
+        function writeMassage() {
+            echo "You are really a nice person, Have a nice time!";
+        }
+        /*Calling a php function*/
+        writeMassage();
+    ?>
+</body>
+</html>
